db5c14fd0007046f92999cc9241f081f28b88883
Frederikus <Frederik.v*******@f*****.be>
2016-02-02 13:42:31 +0100

    Added a RunAllTests funtion to the infratest project

--- client_software/client_software/EidInfraTest/EidInfraTest.cpp
+++ client_software/client_software/EidInfraTest/EidInfraTest.cpp
@@ -9,11 +9,15 @@
 #include <string.h>
 #include <stdio.h>
 #include "util.h"
+#include <time.h>
+#include "testlog.h"
 
 using namespace EidInfra;
 
 char VIRTUAL_CARD_PATH[1024];
 char RELATIVE_PATH_PREFIX[32];
+
+int RunAllTests(void);
 
 int main(int argc, char* argv[])
 {
@@ -32,7 +36,8 @@ int main(int argc, char* argv[])
 
 	strcpy_s(RELATIVE_PATH_PREFIX, 31, "../");
 
-	int result = UnitTest::RunAllTests();
+	int result = 0;
+	RunAllTests();
 
 	return result;
 }
@@ -50,4 +55,86 @@ bool writeFile(const char *filename, const void *contents, size_t contentsLen)
 	fclose(f);
 
 	return true;
+}
+
+typedef	struct {
+	int result;
+	int expectresult;
+	char* testDescription;
+	void (*test_function_ptr)(void);
+} eIDTest;
+
+int RunAllTests()
+{
+	clock_t startTime = clock();
+	clock_t duration = 0;
+	int msecDuration = 0;
+
+	int i = 0;
+	int nrofTests = 0;
+	errno_t error;
+	eIDTest eIDTests[] = { \
+
+	{0,0,"TEST(Transmit) description",&Transmit},
+	{0,0,"TEST(GetVirtualFilePath1) description",&GetVirtualFilePath1},
+	{0,0,"TEST(GetShowFile) description",&GetShowFile},
+	{0,0,"TEST(GetVirtualFilePaths) description",&GetVirtualFilePaths},
+	{0,0,"TEST(GetVirtualFilePathsNoCrLf) description",&GetVirtualFilePathsNoCrLf},
+	{0,0,"TEST(BadControlFiles) description",&BadControlFiles},
+	{0,0,"TEST(addAll) description",&addAll},
+	{0,0,"TEST(selectByPath) description",&selectByPath},
+	{0,0,"TEST(selectByFID) description",&selectByFID},
+	{0,0,"TEST(selectByAID) description",&selectByAID},
+	{0,0,"TEST(loadFromFile) description",&loadFromFile},
+	{0,0,"TEST(Readers) description",&Readers},
+	{0,0,"TEST(Transactions) description",&Transactions},
+	{0,0,"TEST(Properties) description",&Properties},
+	{0,0,"TEST(GetNextElement) description",&GetNextElement},
+	{0,0,"TEST(FindElement) description",&FindElement},
+	{0,0,"TEST(seekToContentElement) description",&seekToContentElement},
+	{0,0,"TEST(BigData) description",&BigData},
+	};
+
+	error = INITLOG();
+	if(error != 0)
+	{
+		printf("Can't open logfile");
+		return -1;
+	}
+	nrofTests = (sizeof(eIDTests)/sizeof(eIDTest));
+
+	while (i < nrofTests )
+	{
+		//eIDTests[i].result = //for now, no testresults yet, only logs
+		eIDTests[i].test_function_ptr();
+		i++;
+	}
+
+	for (i = 0; i < nrofTests; i++)
+	{
+		if (eIDTests[i].testDescription != NULL)
+		{
+			testlog(LVL_NOLEVEL,"_______________________________________________\n");
+			testlog(LVL_NOLEVEL,"Test %d %s \n", i, eIDTests[i].testDescription);
+		}
+//		if(eIDTests[i].result != eIDTests[i].expectresult)
+//		{
+//			testlog(LVL_NOLEVEL,"FAILED : Result = 0x%.8x Expected Result\n", eIDTests[i].result, eIDTests[i].expectresult );
+//		}
+	}
+
+	duration = clock() - startTime;
+
+	msecDuration = (duration * 1000) / CLOCKS_PER_SEC;
+	printf("Duration: %d,%d seconds", msecDuration/1000, msecDuration%1000);
+
+	testlog(LVL_NOLEVEL,"\n===============================================\n");
+
+	ENDLOG();
+	testlog(LVL_NOLEVEL,"press a key to end the test\n");
+	// Wait for user to end this test
+#ifdef WIN32
+	getchar();
+#endif
+	return 0;
 }
